2faf94cd126c77a3707252cc490d02fa65ae6560
Diesel-19 <aselimulgur@gmail.com>
2020-09-24 23:34:31 +0200

    moved code to comment

--- src/automation/Locators.java
+++ src/automation/Locators.java
@@ -12,6 +12,6 @@ public class Locators {
 		driver.get("https://www.facebook.com");
 		driver.findElement(By.id("email")).sendKeys("This is my first code");
 		driver.findElement(By.name("pass")).sendKeys("This is the password");
-		driver.findElement(By.linkText("Forgot Password?")).click();
+//		driver.findElement(By.linkText("Forgot Password?")).click();
 	}
 }
